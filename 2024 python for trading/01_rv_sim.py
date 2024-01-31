25269f599c15b9dda46e21dc8df05adfba9c2745
nachoddiaz <nachodiazutr@gmail.com>
2024-01-31 16:33:55 +0100

    04 done

--- 2024 python for trading/01_rv_sim.py
+++ 2024 python for trading/01_rv_sim.py
@@ -9,12 +9,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
-from scipy.stats import skew, kurtosis, chi2
+from scipy.stats import skew, kurtosis, chi2, tmean, tstd
 
 #inputs
-coef = 2000 #df in student t, scale in esponential = tasa λ = 1/coef
+coef = 5 #df in student t, scale in esponential = tasa λ = 1/coef
 size=10**6 
-random_var_type = 'Chi-squared'
+random_var_type = 'Student t'
 #Destributions: Normal Student t Uniform Exponential Chi-squared
 decimals = 6
 
@@ -37,16 +37,70 @@ elif random_var_type == 'Chi-squared':
     
 mu = np.mean(x) 
 sigma = np.std(x)
-skew = skew(x)
+#skew is positive if the queue in the right is graeter than the left one
+#this is important to know the expected profitability
+skewness = skew(x)
+#kurt measures the probability of black swans / home runs or 
+# the probability of falling into queues
 kurt = kurtosis(x)
 
-str_title += '\n' + 'Mean = ' + str(np.round(mu ,decimals)) \
-    +'\n' + 'Volatility = ' + str(np.round(sigma ,decimals)) \
-    +'\n' + 'Skewness = ' + str(np.round(skew ,decimals)) \
-    +'\n' + 'Kurtosis = ' + str(np.round(kurt ,decimals))
+#Normality Test: Jarque-Bera Test
+#If data comes from a nornmal distribution, the JB test 
+#is distributed asymptotically as a chi square distribution
+#With that test we are going to know how a distribution is related to a normal
+jb_stat= (size/6)*(skewness**2 + 1/4*kurt**2)
+#cdf gives the distribution function, the integral in x
+p_value = 1- chi2.cdf(jb_stat, df=2)
+#p-value 
+is_normal = (p_value > 0.05)
 
+
+str_title += '\n' + 'Mean = ' + str(np.round(mu ,decimals)) \
+    +' | ' + 'Volatility = ' + str(np.round(sigma ,decimals)) \
+    +'\n' + 'Skewness = ' + str(np.round(skewness ,decimals)) \
+    +' | ' + 'Kurtosis = ' + str(np.round(kurt ,decimals)) \
+    +'\n' + 'JB stat = ' + str(np.round(jb_stat ,decimals)) \
+    +' | ' + 'P Value = ' + str(np.round(p_value ,decimals)) \
+    +' \n ' + 'Is Normal = ' + str(is_normal)
 #plot
-plt.figure()
-plt.hist(x, bins=100, density=True)
-plt.title(str_title)
-plt.show()
+ 
+
+#Cálculo de un supuesto donde falla el test
+
+
+
+n = 0
+is_normal = True
+while is_normal and n<100:
+    x = np.random.standard_normal(size)
+    mu = tmean(x) 
+    sigma = tstd(x)
+    skewness = skew(x)
+    kurt = kurtosis(x)
+    jb_stat= (size/6)*(skewness**2 + 1/4*kurt**2)
+    p_value = 1- chi2.cdf(jb_stat, df=2)
+    is_normal = (p_value > 0.05)
+    print('n='+ str(n) +' is normal= ' + str(is_normal))
+    n += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
