cb5d60140f7b2e94457bec25604d524bee223f4e
nachoddiaz <nachodiazutr@gmail.com>
2024-03-05 21:52:47 +0100

    variance and return of the portfolio added

--- 2024 python for trading/09_porfolio.py
+++ 2024 python for trading/09_porfolio.py
@@ -31,12 +31,13 @@ universe = ['^SPX','^IXIC','^MXX','^STOXX','^GDAXI','^FCHI','^VIX',\
             'BTC-USD','ETH-USD','SOL-USD','USDC-USD','USDT-USD','DAI-USD',\
             'EURUSD=X','GBPUSD=X','CHFUSD=X','SEKUSD=X','NOKUSD=X','JPYUSD=X','MXNUSD=X'\
             ]
-    
-rics = random.sample(universe, 5)
-notional = 15
+
+number_rics = 5
+rics = random.sample(universe, number_rics)
+notional = 2000
 
 
-prt_mng = porfolios.manager(rics, notional)
+prt_mng = porfolios.manager(rics, notional, number_rics)
 prt_mng.compute_covariance()
 
 
@@ -44,5 +45,13 @@ prt_mng.compute_covariance()
 port_min_var_L1 = prt_mng.compute_portfolio('min_var_L1')
 port_min_var_L2 = prt_mng.compute_portfolio('min_var_L2')
 port_eq_weigth = prt_mng.compute_portfolio('eq_weigth')
+
+pesos = port_min_var_L1.weights
+varianza_portafolio = np.dot(port_min_var_L1.weights.T, \
+                             np.dot(prt_mng.mtx_var_cov, \
+                                    port_min_var_L1.weights))
+retorno_portfolio = prt_mng.compute_return()
+print("Varianza del Portafolio:", varianza_portafolio)
+print("retorno del Portafolio:", retorno_portfolio)
 
 
--- 2024 python for trading/porfolios.py
+++ 2024 python for trading/porfolios.py
@@ -24,19 +24,28 @@ def portfolio_var(x, mtx_var_cov):
 
 class manager: 
     
-    def __init__(self,rics,notional):
+    def __init__(self,rics,notional, number_rics):
+        self.number_rics = number_rics
         self.rics = rics
         self.notional = notional
         self.mtx_var_cov = None
         self.mtx_correl = None
         self.portfolio_type = None
+        self.mean_annual = []
         
     def compute_covariance(self):
         df = capm.model.sync_returns(self.rics)
         mtx= df.drop(columns=['date'])
         self.mtx_var_cov = np.cov(mtx, rowvar=False) *252
         self.mtx_correl = np.corrcoef(mtx, rowvar=False)
+        
+    def compute_return(self, factor=252):
+        for ric in self.rics:
+            self.timeSeries = market_data.load_timeseries(ric)
+            self.x = self.timeSeries['return'].values
+            self.mean_annual = st.tmean(self.x) * factor
     
+        return self.mean_annual
     
     def compute_portfolio(self, portfolio_type='default'): 
         #inputs
