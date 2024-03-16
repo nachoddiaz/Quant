c2239dc71ed4f4105a4839ec477f1cb3e482f029
nachoddiaz <nachodiazutr@gmail.com>
2024-03-16 21:36:42 +0100

    plotting strategy starts

--- Strategies/RSI/Crypto.py
+++ Strategies/RSI/Crypto.py
@@ -4,10 +4,7 @@ Created on Tue Mar 12 17:02:44 2024
 
 @author: Nacho
 """
-import pandas as pd
-import ccxt
-import mplfinance as mpf
-import pandas_ta as ta
+
 import importlib
 
 import support_functions
@@ -16,34 +13,22 @@ importlib.reload(support_functions)
 import RSI_FIFO
 importlib.reload(RSI_FIFO)
 
-import RSI_LIFO
-importlib.reload(RSI_LIFO) 
 
 symbol = 'ADA/EUR'
 days = 800
 operational_days = 365
 interval = '1d'
 
-datos_preparados = support_functions.manager(symbol, days, interval)
+support = support_functions.manager(symbol, days, interval)
 
-datos_preparados = datos_preparados.prepare_data()
+datos_preparados = support.prepare_data()
 
 rsi_fifo = RSI_FIFO.manager(datos_preparados, symbol)
-rsi_lifo = RSI_LIFO.manager(datos_preparados, symbol)
 
 rsi_fifo.rsi_strategy()
 rsi_fifo.plot_strategie()
 rsi_fifo.implement_strategie()
 rsi_fifo.compute_stats(operational_days)
-
-
-rsi_lifo.rsi_strategy()
-rsi_lifo.plot_strategie()
-rsi_lifo.implement_strategie()
-rsi_lifo.compute_stats(operational_days)
-
-
-
 
 annualized_return = rsi_fifo.annualized_return
 volatility_annual = rsi_fifo.volatility_annual
@@ -53,7 +38,7 @@ print('The annualized return for this FIFO RSI strategy is: '+ str(annualized_re
 print('The volatility for this FIFO RSI strategy is: '+ str(volatility_annual) + ' %')
 
 
-
+plot_result = support.plot_result(rsi_fifo.info)
 
 
 
--- Strategies/RSI/support_functions.py
+++ Strategies/RSI/support_functions.py
@@ -11,40 +11,42 @@ import mplfinance as mpf
 import pandas_ta as ta
 import numpy as np
 import scipy.stats  as st 
-  
+import importlib
+
+import RSI_FIFO
+importlib.reload(RSI_FIFO)
+
         
   
 class manager:
     
     def __init__(self,symbol,days, interval):
-        self.df_done = None
-        self.apds_done = None
         self.symbol = symbol
         self.days = days
         self.interval = interval
-        self.ohlcv = None
-        self.df_symbols = None
-        self.df_position = None
-        self.returns = None
-        self.purchases = []
-        self.sales = []
-        self.daily_return = None
     
     def prepare_data(self):
         exchange = ccxt.binance()
         exchange.load_markets()
         symbols_path = './symbols_binance.xlsx'
-        self.df_symbols = pd.read_excel(symbols_path)
-        self.df_symbols = self.df_symbols['Symbol'].values
+        df_symbols = pd.read_excel(symbols_path)
+        df_symbols = df_symbols['Symbol'].values
 
-        if self.symbol in self.df_symbols:
+        if self.symbol in df_symbols:
              # Get symbol price
-             self.ohlcv = exchange.fetch_ohlcv(self.symbol, self.interval, limit=self.days)
+             ohlcv = exchange.fetch_ohlcv(self.symbol, self.interval, limit=self.days)
         else: print('symbol doesnt exists')
         
-        self.df = pd.DataFrame(self.ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        self.df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         self.df['timestamp'] = pd.to_datetime(self.df['timestamp'], unit='ms')
         self.df.set_index('timestamp', inplace=True)
         
         return self.df
-       
+    
+        
+    def plot_result(self, info):
+        self.info = info
+        
+        
+        
+        
